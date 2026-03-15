f0ca2e0accb7ec67a75c329956d6adb8ff6e70f4
c-mon71 <simon@vidmar.email>
2026-03-15 20:54:13 +0100

    first commit

---
+++ src/htmlnode.py
@@ -0,0 +1,35 @@
+class HTMLNode():
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        props_to_html = ""
+        for p in self.props:
+            props_to_html += f' {p}="{self.props[p]}"'
+        return props_to_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf node must have value")
+        if not self.tag:
+            return self.value
+        if not self.props:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
+
