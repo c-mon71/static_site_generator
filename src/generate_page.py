2d3f5f441bf94bba3d33a3658ea87c447b714208
SimonVid <vidmar.simon@gmail.com>
2026-03-26 08:50:35 +0100

    202603260850

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,6 @@
 from markdown_blocks import markdown_to_html_node
 from title import extract_title
+from copy_dir import write_to_log
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -14,4 +15,16 @@ def generate_page(from_path, template_path, dest_path):
     html_page = html_page_title.replace("{{ Content }}", html)
     with open(dest_path, "w") as html_file:
         html_file.write(html_page)
+    return
+
+def generate_pages(from_path, template_path, dest_path):
+    list_of_objects = listdir(from_path)
+    for obj in list_of_objects:
+        if path.isdir(f"{from_path}/{obj}"):
+            makedirs(f"{dest_path}/{obj}")
+            write_to_log(logfile, f"makedir {dest_path}/{obj}")
+            copy_files_to_dest(f"{from_path}/{obj}",f"{dest_path}/{obj}", logfile)
+        else:
+            write_to_log(logfile, f"generate from {from_path}/{obj} to {from_path}/{obj}")
+            generate_page(f"{from_path}/{obj}",f"{dest_path}/{obj}", logfile)
     return
