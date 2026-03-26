119ca6295339857dae4b9cc975014809f29f47e4
c-mon71 <simon@vidmar.email>
2026-03-26 19:52:09 +0100

    202603261952

--- src/generate_page.py
+++ src/generate_page.py
@@ -6,9 +6,11 @@ from os import(
     makedirs,
     listdir
 )
-def generate_page(from_path, template_path, dest_path):
+def generate_page(prefix, from_path, template_path, dest_path):
     if not from_path.endswith(".md"):
         return
+    from_path = f"{prefix}{from_path}"
+    dest_path = f"{prefix}{dest_path}"
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as md_file:
         md_text = md_file.read()
@@ -17,23 +19,25 @@ def generate_page(from_path, template_path, dest_path):
     node = markdown_to_html_node(md_text)
     html = node.to_html()
     title = extract_title(md_text)
-    html_page_title = template.replace("{{ Title }}", title)
-    html_page = html_page_title.replace("{{ Content }}", html)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace("/href", f"{prefix}href")
     with open(dest_path, "w") as html_file:
-        html_file.write(html_page)
+        html_file.write(template)
     return
 
-def generate_pages(from_path, template_path, dest_path):
-    list_of_objects = listdir(from_path)
+def generate_pages(prefix, from_path, template_path, dest_path):
+    list_of_objects = listdir(f"{prefix}{from_path}")
+    print(f"LOB: {list_of_objects}")
     for obj in list_of_objects:
-        if path.isdir(f"{from_path}/{obj}"):
-            makedirs(f"{dest_path}/{obj}")
+        if path.isdir(f"{prefix}{from_path}/{obj}"):
+            makedirs(f"{prefix}{dest_path}/{obj}")
             # write_to_log(logfile, f"makedir {dest_path}/{obj}")
-            generate_pages(f"{from_path}/{obj}",template_path, f"{dest_path}/{obj}")
+            generate_pages(prefix,f"{from_path}/{obj}",f"{template_path}", f"{dest_path}/{obj}")
         else:
             # write_to_log(logfile, f"generate from {from_path}/{obj} to {from_path}/{obj}")
             out_obj=obj.replace(".md", ".html")
-            generate_page(f"{from_path}/{obj}",template_path,f"{dest_path}/{out_obj}")
+            generate_page(prefix, f"{from_path}/{obj}",f"{template_path}",f"{dest_path}/{out_obj}")
     return
 
 #print(getcwd())
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 #from src.textnode import TextNode
+import sys
+from os import getcwd
+
 from copy_dir import copy_dir
 from generate_page import generate_pages
 
 def main():
-    prefix = "/Users/simonv/Documents/GitHub_c-mon71/static_site_generator"
-    copy_dir(f"{prefix}/static", f"{prefix}/public", "copy_dir.log")
-    generate_pages("content", "template.html", "public")
+    prefix = getcwd()+"/"
+    print(f"prefix: {prefix}")
+    #prefix = "/Users/simonv/Documents/GitHub_c-mon71/static_site_generator"
+    copy_dir(f"{prefix}static", f"{prefix}docs", "copy_dir.log")
+    generate_pages(prefix, "content", "template.html", "docs")
 
 if __name__ == "__main__":
     main()
